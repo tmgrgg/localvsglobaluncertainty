1f0e5612e001a1aaeebb249948c3f6af041f376d
tmgrgg <thomasggrigg@gmail.com>
2020-07-17 11:38:56 +0100

    add SWAG posterior

--- training/swag.py
+++ training/swag.py
@@ -72,3 +72,58 @@ class SWAGSampler:
 
     def zero_grad(self):
         self.optimizer.zero_grad()
+
+
+class SWAGPosterior(torch.nn.Module):
+
+    def __init__(self, model, swag, var_clamp=1e-30):
+        super(SWAGPosterior, self).__init__()
+        self.model = model
+        # each parameter in model.parameters() must map to a sq_mean, mean, and deviations
+        # otherwise SWAG will break!
+        self.state = swag.state
+        self.var_clamp = var_clamp
+        self._infer_sigmas()
+
+    def _infer_sigmas(self):
+        list_mean = []
+        list_sq_mean = []
+        list_deviations = []
+        for p in self.model.parameters():
+            state = self.state[p]
+            if not all(x in state.keys() for x in ['mean', 'sq_mean', 'deviations']):
+                raise RuntimeError('SWAGPosterior is missing required state.')
+
+            # here's the part where we assume that model.parameters() doesn't decide to
+            # change order under the hood...
+            list_mean.append(state['mean'].view(-1))
+            list_sq_mean.append(state['sq_mean'].view(-1))
+            list_deviations.append(state['deviations'].view(state['deviations'].shape[0], -1))
+
+        mean = torch.cat(list_mean).cpu()
+        sq_mean = torch.cat(list_sq_mean).cpu()
+        deviations = torch.cat(list_deviations, dim=1).cpu()
+
+        self.sigma_diag = torch.clamp(sq_mean - mean ** 2, self.var_clamp)
+        self.sigma_low_rank = deviations.t()
+
+    def sample(self, scale=0.5):
+        z1 = torch.randn_like(self.sigma_diag, requires_grad=False)
+        diag_term = self.sigma_diag.sqrt() * z1
+
+        rank = self.sigma_low_rank.shape[1]
+        z2 = torch.randn(rank, requires_grad=False)
+        low_rank_term = self.sigma_low_rank.mv(z2)
+        low_rank_term /= (rank - 1) ** 0.5
+
+        rand_sample = (diag_term + low_rank_term) / (scale ** 0.5)
+
+        i = 0
+        for p in self.model.parameters():
+            shape = p.data.shape
+            n = p.data.numel()
+            p.data = rand_sample[i: i + n].view(shape).to(p.device)
+            i += n
+
+    def forward(self, *input):
+        return self.model(*input)
