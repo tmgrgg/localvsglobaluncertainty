0b19db2b24cfb99cbe06da951c8567bfc3e1f02b
tmgrgg <thomasggrigg@gmail.com>
2020-07-21 14:31:11 +0100

    move into src folder; create data/datasets and populate with FashionMNist for the moment

---
+++ src/data/datasets.py
@@ -0,0 +1,55 @@
+from torchvision.datasets import FashionMNIST
+from torchvision import transforms
+from torch.utils import data
+
+
+DEFAULT_TRANSFORM = transforms.Compose([transforms.ToTensor()])
+
+
+def load_FashionMNIST(
+        path,
+        use_validation=True,
+        val_size=10000,
+        train_transforms=DEFAULT_TRANSFORM,
+        test_transforms=DEFAULT_TRANSFORM,
+        pin_memory=True,
+        batch_size=128,
+        num_workers=1
+):
+    train_set = FashionMNIST(path, train=True, download=True, transform=train_transforms)
+    test_set = FashionMNIST(path, train=False, download=True, transform=test_transforms)
+
+    if use_validation:
+        train_set.data = train_set.data[:-val_size]
+        train_set.targets = train_set.targets[:-val_size]
+        val_set = FashionMNIST(path, train=True, download=True, transform=test_transforms)
+        val_set.train = False
+        val_set.data = val_set.data[-val_size:]
+        val_set.targets = val_set.targets[-val_size:]
+
+    train_loader = data.DataLoader(
+        train_set,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=pin_memory
+    )
+
+    valid_loader = None
+    if use_validation:
+        valid_loader = data.DataLoader(
+            val_set,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=num_workers,
+            pin_memory=pin_memory
+        )
+
+    test_loader = data.DataLoader(
+        test_set,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers
+    )
+
+    return {'train': train_loader, 'valid': valid_loader, 'test': test_loader}
