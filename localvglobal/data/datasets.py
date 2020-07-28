65d1cae093c7b4343768937f8ac9427e298c0476
tmgrgg <thomasggrigg@gmail.com>
2020-07-28 23:27:23 +0100

    change val_size argument to val_ratio

--- localvglobal/data/datasets.py
+++ localvglobal/data/datasets.py
@@ -9,7 +9,7 @@ DEFAULT_TRANSFORM = transforms.Compose([transforms.ToTensor()])
 def load_FashionMNIST(
         dir,
         use_validation=True,
-        val_size=10000,
+        val_ratio=0.2,
         train_transforms=DEFAULT_TRANSFORM,
         test_transforms=DEFAULT_TRANSFORM,
         pin_memory=True,
@@ -21,6 +21,7 @@ def load_FashionMNIST(
     test_set = FashionMNIST(path, train=False, download=True, transform=test_transforms)
 
     if use_validation:
+        val_size = val_ratio * len(train_set)
         train_set.data = train_set.data[:-val_size]
         train_set.targets = train_set.targets[:-val_size]
         val_set = FashionMNIST(path, train=True, download=True, transform=test_transforms)
