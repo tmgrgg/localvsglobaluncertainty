a988e6ab2e5525611babcdf9b2b91527a327babd
tmgrgg <thomasggrigg@gmail.com>
2020-07-29 15:04:39 +0100

    add cuda flag to 1a run scrip

--- results/1a/run.sh
+++ results/1a/run.sh
@@ -1,3 +1,3 @@
 python experiments/train_model \
 --model densenet10 --dataset fashionmnist --dir results/1a --name train_model --optimizer SGD --lr_init 0.1 \
---lr_final 0.005 --l2 1e-4 --momentum 0.85 --save_graph --verbose
+--lr_final 0.005 --l2 1e-4 --momentum 0.85 --save_graph --verbose --cuda 
