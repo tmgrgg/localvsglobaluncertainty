a09b56c6d6b14de07bd131cc5b2e4ef4724bfb53
tmgrgg <thomasggrigg@gmail.com>
2020-07-30 14:18:20 +0100

    include script for running entirety of experiment 1a (currently just contains model training)

--- results/1a/run.sh
+++ results/1a/run.sh
@@ -1,3 +1,4 @@
 python experiments/train_model \
---model densenet10 --dataset fashionmnist --dir results/1a --name train_model --optimizer SGD --lr_init 0.1 \
---lr_final 0.005 --l2 1e-4 --momentum 0.85 --save_graph --verbose --cuda 
+--model densenet10 --dataset fashionmnist --dir $1 --name train_model --optimizer SGD --lr_init 0.1 \
+--lr_final 0.005 --l2 1e-4 --momentum 0.85 --save_graph --verbose --cuda
+
