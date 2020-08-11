b9e928f629cda4158ea158921e131d6c8e38b285
tmgrgg <thomasggrigg@gmail.com>
2020-08-11 02:24:07 +0100

    upgrade experiment utils

--- experiments/utils.py
+++ experiments/utils.py
@@ -1,146 +1,146 @@
-import os
-from datetime import datetime
-import pandas as pd
 from PIL.Image import Image
+import os
+import pandas as pd
 import torch
+from datetime import datetime
 
 
-# what about if we had like an experiments class that just takes a bit of code and shoves it into the run method
-# and has some predefined API with how it tracks and deals with/caches recorded quantities so that so long as the
-# experiments class is instantiated and persisted in the same place it can be rerun without issue...
-# would also possibly help to define experiments as command line tools in a more trivial way? Given that presumably
-# with the way they did it before if there's a crash or you get booted off you have to rerun the experiments from scratch?
-# actually that's not true... they did have that saving stuff. Perhaps that's enough?
-# whta I've written here is for 'short repeated experiments'
+class ExperimentDirectory:
+
+    def __init__(self, dir, name):
+        self.path = os.path.join(dir, name)
+        if not os.path.exists(self.path):
+            print("Experiment directory does not exist, creating experiment directory {}.".format(self.path))
+            os.mkdir(self.path)
+            # add default subdirectories
+            self.add_folder("models")
+            self.add_folder("images")
+        self.tables = {}
+        for name in os.listdir(self.path):
+            path = os.path.join(self.path, name)
+            if os.path.isdir(path):
+                self.__dict__.update({name + '_path': path})
+            if os.path.isfile(path) and name.endswith('.csv'):
+                self.add_table(name[:-4])
+
+    def add_folder(self, name):
+        path = os.path.join(self.path, name)
+        if os.path.exists(path):
+            return path
+        os.mkdir(path)
+        self.__dict__.update({name + '_path': path})
+        return path
+
+    def add_table(self, name):
+        table = ExperimentTable(exp_dir=self, name=name)
+        self.tables.update({name: table})
+
+    def save_state_dict(self, state_dict, name='_', folder='models'):
+        folder_path = self.add_folder(folder)
+        path = os.path.join(folder_path, '{}.pt'.format(name))
+        torch.save(state_dict, path)
+
+    def write(self, result_dict, table_name=None):
+        table = self._infer_table(table_name)
+        table.write(result_dict)
+
+    def cached_state_dict(self, name, folder='models'):
+        folder_path = self.add_folder(folder)
+        path = os.path.join(folder_path, '{}.pt'.format(name))
+        if os.path.isfile(path):
+            print('Loading cached state dict for {} in {}'.format(name, folder_path))
+            return torch.load(path), None
+        return None, lambda state_dict: self.save_state_dict(state_dict, name=name, folder=folder)
+
+    def cached_table_row(self, cache_dict, table_name=None):
+        table = self._infer_table(table_name)
+        df = pd.read_csv(table.path)
+        for key, val in cache_dict.items():
+            df = df[df[key] == val]
+        if len(df) != 0:
+            print('Loading cached table row for {} in {}'.format(cache_dict, table.path))
+            return df, None
+        return None, lambda result_dict: self.write(result_dict.update(cache_dict) or result_dict,
+                                                    table_name=table_name)
+
+    def cached_image(self, name, folder='images'):
+        folder_path = self.add_folder(folder)
+        path = os.path.join(folder_path, '{}.png'.format(name))
+        if os.path.isfile(path):
+            print('Loading cached state dict for {} in {}'.format(name, folder_path))
+            return Image.load(path), None
+        return None, lambda image: image.save(path)
+
+    def _infer_table(self, table_name):
+        if (len(self.tables) > 1) and (table_name is None):
+            raise RuntimeError(
+                "Cannot infer table: multiple tables in ExperimentDirectory. (Specify table argument from: {})"
+                .format(list(self.tables.keys())))
+        elif table_name is None:
+            table = list(self.tables.values())[0]
+        else:
+            try:
+                table = self.tables[table_name]
+            except KeyError as e:
+                print('No such table {}'.format(table_name))
+                raise e
+        return table
 
 
 class ExperimentTable:
 
-    def __init__(self, path, name, safe=False, include_time=True):
+    def __init__(self, exp_dir, name):
         """
-        Class corresponding to a folder contained a single csv table alongside an images subfolder
-        (TODO: handle images in write).
-        :param path: path where experiments folder should persist
-        :param name: name of experiments/experiments folder
-        :param safe: assert that measure values (i.e. keys of dictionary's passed to write) all conform
-                        to the same values as the first call to write.
-        :param include_time: insert the time that the record was written as a measured value
+        Class corresponding to a csv table
+        :param exp_dir: ExperimentDirectory where table should be stored
+        :param name: name of table
         """
-        self.path = path + '/' + name
-        self.csv_path = self.path + '/table.csv'
-        self.image_path = self.path + '/images'
-        self.model_path = self.path + '/models'
-        self._include_time = include_time
+        if not name.endswith('.csv'):
+            name = name + '.csv'
+        self.path = os.path.join(exp_dir.path, name)
 
-        if not os.path.isdir(self.path):
-            print('::: ExperimentTable {} does not exist! :::'.format(self.path))
-            self.__setup_dir__()
-        else:
-            self.__check_dir__()
-
-        self._safe = safe
-        if self._safe:
-            self._columns = []
-
-    def __setup_dir__(self):
-        print('::: Building ExperimentTable :::')
-        os.mkdir(self.path)
-        os.mkdir(self.image_path)
-        os.mkdir(self.model_path)
-        with open(self.path + "/readme.txt", "w") as txtfile:
-            txtfile.write('Describe Experiment in terms of tabulated parameters and context.')
-
-    def write(self, result_dict):
-        if self._include_time:
+    def write(self, result_dict, include_time=True, safe=True):
+        if include_time:
             result_dict['_write_time'] = datetime.now()
 
-        result_dict = self._handle_images(result_dict)
-        result_dict = self._handle_models(result_dict)
-        result_dict = self._handle_model_optim_pairs(result_dict)
-
-
         df = pd.DataFrame([result_dict])
-        if os.path.exists(self.csv_path):
-            if self._safe and set(result_dict.keys()) != self._columns:
-                raise RuntimeError('Experiment Keys do not match! (Disable safe mode if this was intentional)')
-            df.to_csv(self.csv_path, mode='a', header=False, index=False)
+        df = df.reindex(sorted(df.columns), axis=1)
+        if os.path.exists(self.path):
+            df.to_csv(self.path, mode='a', header=False, index=False)
         else:
-            print('Table empty: Creating {}'.format(self.csv_path))
-            df.to_csv(self.csv_path, mode='w', header=True, index=False)
-            if self._safe:
-                self._columns = set(result_dict.keys())
-
-    def __check_dir__(self):
-        # TODO: implement safety checks on folder
-        pass
-
-    def _handle_images(self, result_dict):
-        for (key, val) in result_dict.items():
-            if isinstance(val, Image):
-                k = len(os.listdir(self.image_path))
-                save_path = '{}/{}.png'.format(self.image_path, k)
-                val.save(save_path)
-                result_dict[key] = save_path
-        return result_dict
-
-    def _handle_models(self, result_dict):
-        for (key, val) in result_dict.items():
-            if isinstance(val, torch.nn.Module):
-                print('Saving Model')
-                k = len(os.listdir(self.model_path))
-                save_path = self.get_model_path(k)
-                torch.save(val.state_dict(), save_path)
-                result_dict[key] = save_path
-        return result_dict
-
-    def _handle_model_optim_pairs(self, result_dict):
-        for (key, val) in result_dict.items():
-            if isinstance(val, tuple):
-                if isinstance(val[0], torch.nn.Module) and isinstance(val[1], torch.optim.Optimizer):
-                    print('Saving Model and Optimizer')
-                    k = len(os.listdir(self.model_path))
-                    save_path_model = self.get_model_path(k)
-                    save_path_optim = self.get_optim_path(k)
-                    torch.save(val[0].state_dict(), save_path_model)
-                    torch.save(val[1].state_dict(), save_path_optim)
-                    result_dict[key] = (save_path_model, save_path_optim)
-        return result_dict
-
-    def get_model_path(self, k):
-        return '{}/model_{}.pt'.format(self.model_path, k)
-
-    def get_optim_path(self, k):
-        return '{}/optim_{}.pt'.format(self.model_path, k)
+            print('Creating table: {}'.format(self.path))
+            df.to_csv(self.path, mode='w', header=True, index=False)
 
 
-# For command line: all the setup could go in a setup() function, and then the command line need only parametrise
-# global variables (i.e. across all experiments/in this setup function). A general command line command for an instance
-# of CachedExperiment can then be used and we minimise bash writing.  Not sure this even makes sense.
-class CachedExperiment:
-
-    def __init__(self, table, run, caching_params=[]):
-        assert (type(caching_params) == list)
-        self._table = table
-        self._run = run
-        self._caching_params = caching_params
-
-    def run(self, *args, **kwargs):
-        # any params that are caching params must be passed to run as kwargs. \
-        if len(self._caching_params) > 0:
-            if os.path.exists(self._table.csv_path):
-                df = pd.read_csv(self._table.csv_path)
-                for key in self._caching_params:
-                    val = kwargs[key]
-                    df = df[df[key] == val]
-                if len(df) != 0:
-                    print('Run already completed according to caching_params, skipping call to .run()!')
-                    return
-
-        result_dict = self._run(*args, **kwargs)
-        self._table.write(result_dict)
-
-    def __repr__(self):
-        return self._table.path
+# Deprecated
+# # For command line: all the setup could go in a setup() function, and then the command line need only parametrise
+# # global variables (i.e. across all experiments/in this setup function). A general command line command for an instance
+# # of CachedExperiment can then be used and we minimise bash writing.  Not sure this even makes sense.
+# class CachedExperiment:
+#
+#     def __init__(self, table, run, caching_params=[]):
+#         assert (type(caching_params) == list)
+#         self._table = table
+#         self._run = run
+#         self._caching_params = caching_params
+#
+#     def run(self, *args, **kwargs):
+#         # any params that are caching params must be passed to run as kwargs. \
+#         if len(self._caching_params) > 0:
+#             if os.path.exists(self._table.csv_path):
+#                 df = pd.read_csv(self._table.csv_path)
+#                 for key in self._caching_params:
+#                     val = kwargs[key]
+#                     df = df[df[key] == val]
+#                 if len(df) != 0:
+#                     print('Run already completed according to caching_params, skipping call to .run()!')
+#                     return
+#
+#         result_dict = self._run(*args, **kwargs)
+#         self._table.write(result_dict)
+#
+#     def __repr__(self):
+#         return self._table.path
 
 
 def track(tracker, res_train, res_valid, model='', plot=True):
