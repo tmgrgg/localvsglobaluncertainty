2cd9d8e05857ff6c52f75f6621d2ffd78db3fcab
tmgrgg <thomasggrigg@gmail.com>
2020-08-12 12:24:24 +0100

    add Ensembler class

---
+++ localvglobal/models/utils.py
@@ -0,0 +1,44 @@
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torch
+import numpy as np
+
+
+class Ensembler:
+
+    def __init__(self, data_loader):
+        # do not want data_loader to shuffle!
+        self.data_loader = DataLoader(data_loader.dataset, batch_size=data_loader.batch_size, shuffle=False)
+        self.data_loader = data_loader
+        self.num_datapoints = len(data_loader.dataset)
+        self.num_classes = len(np.unique(data_loader.dataset.targets))
+        self.targets = torch.zeros(size=(self.num_datapoints,)).long()
+        self.predictions = torch.zeros(size=(self.num_datapoints, self.num_classes))
+        self.weights = []
+        self.model_count = 0
+
+    # TODO: handle non-equal weights?
+    @torch.no_grad()
+    def add_model(self, model, activation=lambda output: F.softmax(output, dim=1), eval=True, using_cuda=True):
+        model.eval()
+
+        start_idx = 0
+        for i, (input, target) in enumerate(self.data_loader):
+            batch_size = input.size()[0]
+            end_idx = start_idx + batch_size
+
+            if using_cuda:
+                input = input.cuda(non_blocking=True)
+
+            output = model(input).cpu()
+            self.predictions[start_idx:end_idx] = (self.model_count * self.predictions[start_idx:end_idx] + activation(
+                output)) / (self.model_count + 1)
+            if self.model_count == 0:
+                self.targets[start_idx:end_idx] = target
+
+            start_idx = end_idx
+
+        self.model_count += 1
+
+    def evaluate(self, f):
+        return f(self.predictions, self.targets)
