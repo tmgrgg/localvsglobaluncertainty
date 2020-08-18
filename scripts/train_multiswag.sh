0915e3ca502e5e382d0ee3d8bac4260bdda5f77e
tmgrgg <thomasggrigg@gmail.com>
2020-08-18 17:20:09 +0100

    fix script

--- scripts/train_multiswag.sh
+++ scripts/train_multiswag.sh
@@ -18,8 +18,8 @@ source ${HOME}/.activate_conda
 cd $HOME/localvsglobaluncertainty
 
 
-test $SGE_TASK_ID -eq 1 && sleep 10 && PYTHONPATH=. python3 experiments/train_multiswag/experiment.py --dir='/content/drive/My Drive/LocalVsGlobalUncertainty/Experiments/FashionMNIST/DenseNet/2f' \
---name=train_multiswag --dataset=FashionMNIST --model=DenseNet10 --criterion=CrossEntropyLoss \
+test $SGE_TASK_ID -eq 1 && sleep 10 && PYTHONPATH=. python3 experiments/train_multiswag/experiment.py --dir=$HOME/Results \
+--name=multiswag_heatmap --dataset=FashionMNIST --model=DenseNet10 --criterion=CrossEntropyLoss \
 --batch_size=128 --optimizer=SGD --lr_init=0.1 --lr_final=0.01 --l2=3e-4 --momentum=0.9 \
 --training_epochs=150 --swag_epochs=150 --sample_rate=1.0 --rank=90 --num_models=90 --cuda \
 > out.log 2>&1
