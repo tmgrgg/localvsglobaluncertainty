68b577d98ba5d16f4f0fad5cd7ab29e05f1ce6df
tmgrgg <thomasggrigg@gmail.com>
2020-08-18 15:56:05 +0100

    add scripts for running on cluster

--- scripts/template.sh
+++ scripts/template.sh
@@ -1,4 +1,28 @@
-!python experiments/train_multiswag/experiment.py --dir='/content/drive/My Drive/LocalVsGlobalUncertainty/Experiments/FashionMNIST/DenseNet/2f' \
---name=train_multiswag --dataset=FashionMNIST --model=DenseNet10 --criterion=CrossEntropyLoss \
---batch_size=128 --optimizer=SGD --lr_init=0.1 --lr_final=0.01 --l2=3e-4 --momentum=0.9 \
---training_epochs=150 --swag_epochs=150 --sample_rate=1.0 --rank=90 --num_models=90 --cuda
+#!/bin/bash -l
+ 
+#$ -cwd
+#$ -S /bin/bash
+#$ -o $HOME/array.out
+#$ -e $HOME/array.err
+#$ -t 1-10
+#$ -l tmem=16G
+#$ -l h_rt=24:00:00
+#$ -l gpu=true
+ 
+export LANG="en_US.utf8"
+export LANGUAGE="en_US:en"
+ 
+# Activate the correct gpu environment
+source ${HOME}/.activate_conda
+ 
+cd $HOME/workspace/your-gpu-project
+ 
+
+test $SGE_TASK_ID -eq 1 && sleep 10 && PYTHONPATH=. python ./bin/your-project-cli.py --hyperparams 1
+test $SGE_TASK_ID -eq 2 && sleep 10 && PYTHONPATH=. python ./bin/your-project-cli.py --hyperparams 2
+test $SGE_TASK_ID -eq 3 && sleep 10 && PYTHONPATH=. python ./bin/your-project-cli.py --hyperparams 3
+test $SGE_TASK_ID -eq 4 && sleep 10 && PYTHONPATH=. python ./bin/your-project-cli.py --hyperparams 4
+test $SGE_TASK_ID -eq 5 && sleep 10 && PYTHONPATH=. python ./bin/your-project-cli.py --hyperparams 5
+ 
+# The counter $SGE_TASK_ID will be incremented up to 10; 1 for each job
+[..]
