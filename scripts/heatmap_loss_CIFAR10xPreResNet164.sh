8ee35fc100da47ddf4ffdadf8e5e2c0f07a5f793
tmgrgg <thomasggrigg@gmail.com>
2020-09-03 14:38:25 +0100

    split up jobs for heatmap multiswag30x30_PreResNet164_CIFAR10

--- scripts/heatmap_loss_CIFAR10xPreResNet164.sh
+++ scripts/heatmap_loss_CIFAR10xPreResNet164.sh
@@ -4,7 +4,7 @@
 #$ -S /bin/bash
 #$ -o $HOME/array.out
 #$ -e $HOME/array.err
-#$ -t 1-5
+#$ -t 1-10
 #$ -l tmem=16G
 #$ -l h_rt=48:00:00
 #$ -l gpu=true
@@ -19,28 +19,55 @@ cd $HOME/localvsglobaluncertainty
 
 test $SGE_TASK_ID -eq 1 && sleep 10 && PYTHONPATH=. python3 experiments/heatmap_from_models/experiment.py \
 --dir=$HOME/Results --name=multiswag30x30_PreResNet164_CIFAR10 --dataset=CIFAR10 --model=PreResNet164 \
---criterion=CrossEntropyLoss --batch_size=128 --min_rank=2 --max_rank=30 --max_num_models=30 \
---local_samples=30 --table_seed=1 --verbose --cuda --on_test  > out_heatmap1.log 2>&1
+--criterion=CrossEntropyLoss --batch_size=128 --min_rank=2 --max_rank=15 --max_num_models=30 \
+--local_samples=30 --table_seed=1 --verbose --cuda --on_test  > out_heatmap1a.log 2>&1
 
 test $SGE_TASK_ID -eq 2 && sleep 10 && PYTHONPATH=. python3 experiments/heatmap_from_models/experiment.py \
 --dir=$HOME/Results --name=multiswag30x30_PreResNet164_CIFAR10 --dataset=CIFAR10 --model=PreResNet164 \
---criterion=CrossEntropyLoss --batch_size=128 --min_rank=2 --max_rank=30 --max_num_models=30 \
---local_samples=30 --table_seed=2 --verbose --cuda --on_test  > out_heatmap2.log 2>&1
+--criterion=CrossEntropyLoss --batch_size=128 --min_rank=16 --max_rank=30 --max_num_models=30 \
+--local_samples=30 --table_seed=1 --verbose --cuda --on_test  > out_heatmap1b.log 2>&1
 
 test $SGE_TASK_ID -eq 3 && sleep 10 && PYTHONPATH=. python3 experiments/heatmap_from_models/experiment.py \
 --dir=$HOME/Results --name=multiswag30x30_PreResNet164_CIFAR10 --dataset=CIFAR10 --model=PreResNet164 \
---criterion=CrossEntropyLoss --batch_size=128 --min_rank=2 --max_rank=30 --max_num_models=30 \
---local_samples=30 --table_seed=3 --verbose --cuda --on_test  > out_heatmap3.log 2>&1
+--criterion=CrossEntropyLoss --batch_size=128 --min_rank=2 --max_rank=15 --max_num_models=30 \
+--local_samples=30 --table_seed=2 --verbose --cuda --on_test  > out_heatmap2a.log 2>&1
+
 
 test $SGE_TASK_ID -eq 4 && sleep 10 && PYTHONPATH=. python3 experiments/heatmap_from_models/experiment.py \
 --dir=$HOME/Results --name=multiswag30x30_PreResNet164_CIFAR10 --dataset=CIFAR10 --model=PreResNet164 \
---criterion=CrossEntropyLoss --batch_size=128 --min_rank=2 --max_rank=30 --max_num_models=30 \
---local_samples=30 --table_seed=4 --verbose --cuda --on_test  > out_heatmap4.log 2>&1
+--criterion=CrossEntropyLoss --batch_size=128 --min_rank=16 --max_rank=30 --max_num_models=30 \
+--local_samples=30 --table_seed=2 --verbose --cuda --on_test  > out_heatmap2b.log 2>&1
+
 
 test $SGE_TASK_ID -eq 5 && sleep 10 && PYTHONPATH=. python3 experiments/heatmap_from_models/experiment.py \
 --dir=$HOME/Results --name=multiswag30x30_PreResNet164_CIFAR10 --dataset=CIFAR10 --model=PreResNet164 \
---criterion=CrossEntropyLoss --batch_size=128 --min_rank=2 --max_rank=30 --max_num_models=30 \
---local_samples=30 --table_seed=5 --verbose --cuda --on_test  > out_heatmap5.log 2>&1
+--criterion=CrossEntropyLoss --batch_size=128 --min_rank=2 --max_rank=15 --max_num_models=30 \
+--local_samples=30 --table_seed=3 --verbose --cuda --on_test  > out_heatmap3a.log 2>&1
+
+test $SGE_TASK_ID -eq 6 && sleep 10 && PYTHONPATH=. python3 experiments/heatmap_from_models/experiment.py \
+--dir=$HOME/Results --name=multiswag30x30_PreResNet164_CIFAR10 --dataset=CIFAR10 --model=PreResNet164 \
+--criterion=CrossEntropyLoss --batch_size=128 --min_rank=16 --max_rank=30 --max_num_models=30 \
+--local_samples=30 --table_seed=3 --verbose --cuda --on_test  > out_heatmap3b.log 2>&1
+
+test $SGE_TASK_ID -eq 7 && sleep 10 && PYTHONPATH=. python3 experiments/heatmap_from_models/experiment.py \
+--dir=$HOME/Results --name=multiswag30x30_PreResNet164_CIFAR10 --dataset=CIFAR10 --model=PreResNet164 \
+--criterion=CrossEntropyLoss --batch_size=128 --min_rank=2 --max_rank=15 --max_num_models=30 \
+--local_samples=30 --table_seed=4 --verbose --cuda --on_test  > out_heatmap4a.log 2>&1
+
+test $SGE_TASK_ID -eq 8 && sleep 10 && PYTHONPATH=. python3 experiments/heatmap_from_models/experiment.py \
+--dir=$HOME/Results --name=multiswag30x30_PreResNet164_CIFAR10 --dataset=CIFAR10 --model=PreResNet164 \
+--criterion=CrossEntropyLoss --batch_size=128 --min_rank=16 --max_rank=30 --max_num_models=30 \
+--local_samples=30 --table_seed=4 --verbose --cuda --on_test  > out_heatmap4b.log 2>&1
+
+test $SGE_TASK_ID -eq 9 && sleep 10 && PYTHONPATH=. python3 experiments/heatmap_from_models/experiment.py \
+--dir=$HOME/Results --name=multiswag30x30_PreResNet164_CIFAR10 --dataset=CIFAR10 --model=PreResNet164 \
+--criterion=CrossEntropyLoss --batch_size=128 --min_rank=2 --max_rank=15 --max_num_models=30 \
+--local_samples=30 --table_seed=5 --verbose --cuda --on_test  > out_heatmap5a.log 2>&1
+
+test $SGE_TASK_ID -eq 10 && sleep 10 && PYTHONPATH=. python3 experiments/heatmap_from_models/experiment.py \
+--dir=$HOME/Results --name=multiswag30x30_PreResNet164_CIFAR10 --dataset=CIFAR10 --model=PreResNet164 \
+--criterion=CrossEntropyLoss --batch_size=128 --min_rank=16 --max_rank=30 --max_num_models=30 \
+--local_samples=30 --table_seed=5 --verbose --cuda --on_test  > out_heatmap5b.log 2>&1
 
 # The counter $SGE_TASK_ID will be incremented up to 10; 1 for each job
 [..]
