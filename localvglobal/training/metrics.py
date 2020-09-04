7f6987b71851aa27a39ca4e92e7a2fa007784e89
tmgrgg <thomasggrigg@gmail.com>
2020-09-04 14:48:25 +0100

    add metrics to repo

---
+++ localvglobal/training/metrics.py
@@ -0,0 +1,24 @@
+import torch
+
+
+def matrix_kl(P, Q):
+    # P.size: [num_examples, num_classes] (holding probability predictions)
+    # Q.size: [num_examples, num_classes] (holding probabilitiy predictions)
+    # assuming same order:
+    log_p_over_q = torch.log(P / Q)
+    # stop this from being negative infinity when P = 0
+    log_p_over_q[P == 0] = 0.0
+    # elementwise multiply
+    res = P * log_p_over_q
+    return res.sum(axis=1)
+
+
+def reverse_matrix_KL(P, Q):
+    return matrix_kl(Q, P)
+
+
+def accuracy(outputs, targets):
+    num_datapoints = targets.size(0)
+    preds = outputs.data.argmax(1, keepdim=True)
+    correct = preds.eq(targets.data.view_as(preds)).sum().item()
+    return 100.0 * correct / num_datapoints
